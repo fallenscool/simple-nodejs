9e226e6ee770150fedfd6540f48bf724bd21a09a
Spivachevsky Vladimir <spivachevsky.v@gmail.com>
2019-07-13 23:12:55 +0300

    changed greeting

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.send({
-    hi: 'there'
+    bye: "buddy"
   });
 });
 
