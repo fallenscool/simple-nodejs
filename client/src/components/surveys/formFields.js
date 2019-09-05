6caec430b2aa8cca616a4127becb888a767b8e18
Spivachevsky Vladimir <spivachevsky.v@gmail.com>
2019-09-05 11:50:55 +0300

    finish send email

---
+++ client/src/components/surveys/formFields.js
@@ -0,0 +1,6 @@
+export default [
+  { label: "Survey Title", name: "title", error: "Provide a survey title" },
+  { label: "Subject Line", name: "subject", error: "Provide a survey subject" },
+  { label: "Email Body", name: "body", error: "Provide a body" },
+  { label: "Recipient List", name: "recipients", error: "You must provide an emails" }
+];
